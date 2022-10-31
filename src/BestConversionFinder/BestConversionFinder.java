10aef3aafb64dfff35136655ceee1c4391125591
AfroComrade <72281933+AfroComrade@users.noreply.github.com>
2022-10-31 14:48:31 +1300

    Update conversion finder with better coding practices

--- src/BestConversionFinder/BestConversionFinder.java
+++ src/BestConversionFinder/BestConversionFinder.java
@@ -10,16 +10,17 @@ import java.util.Map;
 import java.util.Set;
 
 /*
-    From my brief understanding, the weights have been added as a map to each edge, 
-    so that we can determine weights similar to the bellman ford example in the lectures
+    This is an attempt at an adjacency graph. This finds the best conversion rate but
+    expectedly breaks with with negative weigbht cycles.
+    Negative weight cycles are arbitrage, but they're not handled in this implementation.
 */
 
-public class BestConversionFinderYeran<E>
+public class BestConversionFinder<E>
 {
     private GraphADT<E> graph;
     private Map<Edge<E>,Float> weights;
 
-    public BestConversionFinderYeran(GraphADT<E> graph, Map<Edge<E>,Float> weights)
+    public BestConversionFinder(GraphADT<E> graph, Map<Edge<E>,Float> weights)
     {  
         this.graph = graph;
         this.weights = weights;
@@ -30,36 +31,40 @@ public class BestConversionFinderYeran<E>
         Map<Vertex<E>,Edge<E>> leastEdges = new HashMap<Vertex<E>,Edge<E>>();
         final Map<Vertex<E>,Float> ValuablePathEstimates = new HashMap<Vertex<E>,Float>();
 
+        //  Assign infinity to every vertex that isn't the source 
         for (Vertex v : graph.vertexSet())
         {
-            //System.out.println("putting " + 0 + "into " + v);
             ValuablePathEstimates.put(v, (float)(-Math.log(0)));
             leastEdges.put(v, null);
         }
         
+        //  Assign 0 to the source
         ValuablePathEstimates.put(source, (0.0f));
         int n = graph.vertexSet().size();
         
+        // Go through each edge, and relax once for each vertex
         for (int i = 1; i < n; i++)
         {
             for (Edge e : graph.edgeSet())
             {
                 Vertex start = e.endVertices()[0];
                 Vertex end = e.endVertices()[1];
+                
+                // Get the estimate for the current path for each vertex
                 float value1Toln = ValuablePathEstimates.get(start);
                 float value2Toln = weights.get(e);
                 float value3Toln = ValuablePathEstimates.get(end);
                 
+                // Use math.log to convert currency to a value that can be added, and fits with bellman ford
                 float placeholderValue1 = value1Toln;
                 float placeholderValue2 = (float)(Math.log(1/value2Toln));
                 float placeholderValue3 = value3Toln;
 
-                System.out.println("comparing " + start + " against " + end);
-                System.out.println("Comparing " + value1Toln + " + " + value2Toln + " against " + value3Toln);
-                System.out.println("Comparing " + placeholderValue1 + " + " + placeholderValue2 + " against " + placeholderValue3);
+                //  If the path to the current start of the edge, plus the weight of that edge,
+                //    is shorter than the path that currently exists to the end of the edge, assign it into the value path estimate
+                //    for that vertex
                 if (((placeholderValue1 + placeholderValue2) < placeholderValue3) && (placeholderValue3 != 0.0f))
                 {
-                    System.out.println("  ~adding " + start + " against " + end);
                     if (end != source)
                     {
                         ValuablePathEstimates.put(end, (ValuablePathEstimates.get(start) + placeholderValue2));
@@ -69,27 +74,30 @@ public class BestConversionFinderYeran<E>
             }
         }
         
-        
-        /*
         for (Edge e : graph.edgeSet())
         {
             Vertex u = e.endVertices()[0];
             Vertex v = e.endVertices()[1];
+            
+            //  If, after relaxing all the edges as much as possible, we can still relax edges pointing
+            //  to a vertex, then by definition we have a negative path cycle.
             if (ValuablePathEstimates.get(u) + weights.get(e) < ValuablePathEstimates.get(v))
             {
-                System.out.println("Returning null");
+                System.out.println("Negative Cycle path found, returning null");
                 return null;
             }
         }
-        */
         
         return new HashSet(leastEdges.values());
     }
     
     public static void main(String[] args)
     {  
+        // easyTest1 has a small graph and works because it has no arbitrage
         easyTest1();
-        //hardTest1();
+        
+        // hardTest1 has a large graph that has arbitrage, so it breaks when attempting to find the best conversion finder
+        hardTest1();
    }
     
     public static void hardTest1()
@@ -157,76 +165,58 @@ public class BestConversionFinderYeran<E>
             }
         }
         
-        System.out.println("Graph: " + graph);
-        for (Float f : weights.values())
-        {
-            System.out.println(f + " ");
-        }
-        
-        BestConversionFinderYeran<String> Bellman = new BestConversionFinderYeran<String>(graph, weights);
+        BestConversionFinder<String> Bellman = new BestConversionFinder<String>(graph, weights);
         System.out.println("\n\nNew shortestpath for HKD:");
         
         System.out.println(HKD);
-       for (Edge<String> edge : Bellman.getShortestPathsTree(HKD))
-       {
-            System.out.print(" "+edge);
-       }
-       System.out.println();
+        for (Edge<String> edge : Bellman.getShortestPathsTree(HKD))
+        {
+             System.out.print(" "+edge);
+        }
+        System.out.println();
     }
     
     public static void easyTest1()
     {
-                GraphADT<String> graph = new AdjacencyListGraph<String>(GraphADT.GraphType.DIRECTED);
+        GraphADT<String> graph = new AdjacencyListGraph<String>(GraphADT.GraphType.DIRECTED);
         
         Vertex<String> USD = graph.addVertex("USD");
         Vertex<String> CAD = graph.addVertex("CAD");
         Vertex<String> EUR = graph.addVertex("EUR");
         Vertex<String> GBP = graph.addVertex("GBP");
-        /*
-        Vertex<String> HKD = graph.addVertex("HKD");
-        Vertex<String> CHF = graph.addVertex("CHF");
-        Vertex<String> JPY = graph.addVertex("JPY");
-        Vertex<String> AUD = graph.addVertex("AUD");
-        Vertex<String> INR = graph.addVertex("INR");
-        Vertex<String> CNY = graph.addVertex("CNY");
-        Vertex<String> BTC = graph.addVertex("BTC");
-        */
 
-       Edge<String> USDCAD = graph.addEdge(USD, CAD);
-       Edge<String> USDEUR = graph.addEdge(USD, EUR);
-       Edge<String> CADGBP= graph.addEdge(CAD, GBP);
-       Edge<String> EURGBP = graph.addEdge(EUR, GBP);
-       Edge<String> CADUSD = graph.addEdge(CAD, USD);
-       Edge<String> EURUSD = graph.addEdge(EUR, USD);
-       Edge<String> GBPCAD = graph.addEdge(GBP, CAD);
-       Map<Edge<String>,Float> weights = new HashMap<Edge<String>,Float>();
-       weights.put(USDCAD, 1.36f);
-       weights.put(USDEUR, 1.0f);
-       weights.put(CADGBP, 0.64f);
-       weights.put(EURGBP, 0.87f);
-       weights.put(CADUSD, 0.75f);
-       weights.put(EURUSD, 1.0f);
-       weights.put(GBPCAD, 1.56f);
-       System.out.println("Example Graph:\n" + graph);
-       BestConversionFinderYeran<String> Bellman = new BestConversionFinderYeran<String>(graph, weights);
-       System.out.println("Finding shortest paths tree from a");
-       System.out.print("Edges:");
+        Edge<String> USDCAD = graph.addEdge(USD, CAD);
+        Edge<String> USDEUR = graph.addEdge(USD, EUR);
+        Edge<String> CADGBP= graph.addEdge(CAD, GBP);
+        Edge<String> EURGBP = graph.addEdge(EUR, GBP);
+        Edge<String> CADUSD = graph.addEdge(CAD, USD);
+        Edge<String> EURUSD = graph.addEdge(EUR, USD);
+        Edge<String> GBPCAD = graph.addEdge(GBP, CAD);
+        Map<Edge<String>,Float> weights = new HashMap<Edge<String>,Float>();
+        weights.put(USDCAD, 1.36f);
+        weights.put(USDEUR, 1.0f);
+        weights.put(CADGBP, 0.64f);
+        weights.put(EURGBP, 0.87f);
+        weights.put(CADUSD, 0.75f);
+        weights.put(EURUSD, 1.0f);
+        weights.put(GBPCAD, 1.56f);
+        System.out.println("Example Graph:\n" + graph);
+        BestConversionFinder<String> Bellman = new BestConversionFinder<String>(graph, weights);
+
+        /*
+             This will find the lowest transfer rate, from what I understand, from the source
+             So the lowest rate from USD to each currency will be determined
+        */
        
-       /*
-            This will find the lowest transfer rate, from what I understand, from the source
-            So the lowest rate from USD to each currency will be determined
-       */
-      
-       /*
         System.out.println("\n\nNew shortestpath for USD:");
-       for (Edge<String> edge : Bellman.getShortestPathsTree(USD))
-           System.out.print(" "+edge);
-       System.out.println();
-       */
+        for (Edge<String> edge : Bellman.getShortestPathsTree(USD))
+            System.out.print(" "+edge);
+        System.out.println();
+       
        
         System.out.println("\n\nNew shortestpath for EUR:");
-       for (Edge<String> edge : Bellman.getShortestPathsTree(EUR))
-           System.out.print(" "+edge);
-       System.out.println();
+        for (Edge<String> edge : Bellman.getShortestPathsTree(EUR))
+            System.out.print(" "+edge);
+        System.out.println();
     }
 }
